238ca6fc5cbeddf0aa6dea7f477b31898c329840
julien <julien@air-de-admin-2.home>
2023-03-22 08:59:01 +0100

    suite de l'initiation

---
+++ src/Form/IngredientType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class IngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ],
+                ])
+            ->add('price', NumberType::class, [
+                'label' => 'Prix',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Prix en centimes',
+                    ],
+                ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ingredient::class,
+        ]);
+    }
+}
