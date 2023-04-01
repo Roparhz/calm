9a3f38e0c1acb1dd30fba9d5bd61c4ff622ddc4a
julien <julien@air-de-admin.home>
2023-04-01 16:34:44 +0200

    ajout du formulaire ingrédient

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -3,12 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Ingredient;
-use Doctrine\DBAL\Types\TextType;
+//use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 class IngredientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
